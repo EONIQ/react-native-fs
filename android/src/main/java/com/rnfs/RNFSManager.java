a7474876ae19dc369eb307a9bd1195c0d79b7ace
taifok <fktalbert@gmail.com>
2018-08-30 20:57:49 +0800

    fix - missing import ReadableMapKeySetIterator

--- android/src/main/java/com/rnfs/RNFSManager.java
+++ android/src/main/java/com/rnfs/RNFSManager.java
@@ -19,6 +19,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.RCTNativeAppEventEmitter;
